878dac09920d29ee54acd42f4941f626292b98d3
PasPerfection <pasperfection@outlook.com>
2024-09-26 22:28:10 +0000

    database aanpassing

--- TempleBackend/src/config/database.ts
+++ TempleBackend/src/config/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
   logging: false, // Set to console.log to see SQL queries
